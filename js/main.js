7083cf86c7e143d2a26e81634f2d49db2365b4b4
John Brilla <jbrilla17@gmail.com>
2015-10-15 21:21:10 +1100

    tightened code/sphere

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-var scene, camera, renderer, geometry, cube, material;
-
+var scene, camera, renderer;
+var cubeGeometry, cube, material;
+var sphereGeometry, sphere;
 
 scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
@@ -11,51 +12,53 @@ document.body.appendChild( renderer.domElement );
 camera.position.z = 5;
 camera.position.x = 5;
 
-var cubeHolder = [];
 
-geometry = new THREE.BoxGeometry( 1, 1, 1 );
-material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
+//OBJECTS
+cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+sphereGeometry = new THREE.SphereGeometry( 1, 5, 5 );
+material = new THREE.MeshBasicMaterial( { name: 'green-wire', color: 0x00ff00, wireframe: true } );
+phongMaterial = new THREE.MeshPhongMaterial({ color: 0x156289, emissive: 0x072534, side: THREE.DoubleSide, shading: THREE.FlatShading });
 
-
-
-var cubeRotation = false;
+sphere = new THREE.Mesh( sphereGeometry, phongMaterial );
+// scene.add( sphere );
+//var cubeRotation = false;
+var cubeHolder = []; 					//holds some cube info
 
 var cubeCreate = function(cubeName) {
 	this.cubeName = cubeName;
 	this.cubeRotation = true;
 
-	this.cube = new THREE.Mesh( geometry, material );
+	this.cube = new THREE.Mesh( cubeGeometry, material );
 	scene.add( this.cube );
 
 	return this.cubeName;
 }
 
+var cubeRemove = function () {
+	//removes all cubes
+	scene.children = [];
+	cubeHolder = [];
+}
 
-
+//RENDER & ANIMATE SCENE
 var render = function () {
 	requestAnimationFrame( render );
 
 	for (var i = 0; i < scene.children.length; i++ ) {
 
-		scene.children[i].rotation.x += Math.random() * .1;
-		scene.children[i].rotation.y += 0.01;
-		scene.children[i].position.x += 0.01;
+		if (scene.children[i].type === "Mesh" && scene.children[i].geometry.type === 'BoxGeometry') {
+
+			scene.children[i].rotation.x += Math.random() * .1;
+			scene.children[i].rotation.y += 0.01;
+			scene.children[i].position.x += 0.01;
+
+		}
+		
 		
 	}
-
-
 	
-
-	
-	// console.log(cubeHolder);
 	renderer.render(scene, camera);
 };
-
-
-var removeBoxes = function () {
-	scene.children = [];
-	cubeHolder = [];
-}
 
 $(document).ready( function() {
 
@@ -65,13 +68,13 @@ $(document).ready( function() {
 
 		var a = new cubeCreate(cubeHolder.length.toString());
 		cubeHolder.push(a);
-		console.log(cubeHolder);
+		console.log('CUBE HOLDER Length:', cubeHolder.length);
 
 	});
 
 	$('#removeBoxes').on('click', function () {
 
-		removeBoxes();
+		cubeRemove();
 
 	});
 
@@ -79,12 +82,15 @@ $(document).ready( function() {
 		var a = new cubeCreate(cubeHolder.length.toString());
 		cubeHolder.push(a);
 		console.log('cube has been created');
+		console.log('CUBE HOLDER Length:', cubeHolder.length);
 
 		if (cubeHolder.length === 8 ){
+
 			cubeHolder.shift();
 			scene.children.shift();
+			console.log('A CUBE HAS BEEN REMOVED');
 
-			// removeBoxes();
+			// cubeRemove();
 		}
 	}, 3000);
 
