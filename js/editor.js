4c9bd16aee9feaf8a6b5059c245790329b978513
John Brilla <jbrilla17@gmail.com>
2015-10-17 16:59:57 +1100

    shadows casting

--- js/editor.js
+++ js/editor.js
@@ -14,48 +14,14 @@ camera.position.x = 5;
 
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-var light = new THREE.AmbientLight( 0x404040 ); // soft white light
-scene.add( light );
 
-var spotLight = new THREE.SpotLight( 0xffffff );
-spotLight.position.set( 1, 20, 10 );
-spotLight.castShadow = true;
-spotLight.shadowCameraNear = 8;
-spotLight.shadowCameraFar = 30;
-spotLight.shadowMapWidth = 1024;
-spotLight.shadowMapHeight = 1024;
-spotLight.angle = 1;
-spotLight.exponent = 5;
-scene.add( spotLight );
 
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.BasicShadowMap;
 
 //OBJECTS
-	//Materials
-phongMaterial = new THREE.MeshPhongMaterial({ color: 0x156289, emissive: 0x072534 });
-material = new THREE.MeshBasicMaterial( { name: 'green-wire', color: 0x00ff00, wireframe: true } );
-
-var planeGeometry = new THREE.PlaneGeometry( 100, 100, 1 );
-var planeMaterial = new THREE.MeshPhongMaterial( {color: 0xffffff, 
-												emissive: 0x072534, 
-												side: THREE.DoubleSide, 
-												shading: THREE.FlatShading} );
-
-
-var ground = new THREE.Mesh( planeGeometry, planeMaterial );
-ground.rotation.x = 1.57;
-ground.position.y = -2;
-ground.scale.multiplyScalar( 3 );
-ground.castShadow = false;
-ground.receiveShadow = true;
-scene.add( ground );
-
-
 cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
 sphereGeometry = new THREE.SphereGeometry( 1, 5, 5 );
-
-
+material = new THREE.MeshBasicMaterial( { name: 'green-wire', color: 0x00ff00, wireframe: true } );
+phongMaterial = new THREE.MeshPhongMaterial({ color: 0x156289, emissive: 0x072534, side: THREE.DoubleSide, shading: THREE.FlatShading });
 
 sphere = new THREE.Mesh( sphereGeometry, phongMaterial );
 // scene.add( sphere );
@@ -66,8 +32,7 @@ var cubeCreate = function(cubeName) {
 	this.cubeName = cubeName;
 	this.cubeRotation = true;
 
-	this.cube = new THREE.Mesh( cubeGeometry, phongMaterial );
-	this.cube.castShadow = true;
+	this.cube = new THREE.Mesh( cubeGeometry, material );
 	scene.add( this.cube );
 
 	return this.cubeName;
@@ -75,6 +40,7 @@ var cubeCreate = function(cubeName) {
 
 var cubeRemove = function () {
 	//removes all cubes
+	
 	scene.children = [];
 	cubeHolder = [];
 }
@@ -95,7 +61,7 @@ var render = function () {
 		
 		
 	}
-	
+	controls.update();
 	renderer.render(scene, camera);
 };
 
@@ -120,14 +86,14 @@ $(document).ready( function() {
 	setInterval(function() {
 		var a = new cubeCreate(cubeHolder.length.toString());
 		cubeHolder.push(a);
-		// console.log('cube has been created');
-		// console.log('CUBE HOLDER Length:', cubeHolder.length);
+		console.log('cube has been created');
+		console.log('CUBE HOLDER Length:', cubeHolder.length);
 
 		if (cubeHolder.length === 8 ){
 
 			// cubeHolder.shift();
 			// scene.children.shift();
-			// console.log('A CUBE HAS BEEN REMOVED');
+			console.log('A CUBE HAS BEEN REMOVED');
 
 			// cubeRemove();
 		}
@@ -135,3 +101,18 @@ $(document).ready( function() {
 
 });
 
+
+
+var loader = new THREE.ObjectLoader();
+
+// load a resource
+loader.load(
+	// resource URL
+	'js/models/scene.js',
+	// Function when resource is loaded
+	function ( object ) {
+		// var material = new THREE.MeshFaceMaterial( materials );
+		// var object = new THREE.Mesh( geometry, material );
+		scene.add( object );
+	}
+);
