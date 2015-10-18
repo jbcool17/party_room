e8dd79119fc085c2f94ece1526570a6d4adcc66b
John Brilla <jbrilla17@gmail.com>
2015-10-19 09:47:32 +1100

    update interactions

--- js/interaction.js
+++ js/interaction.js
@@ -14,21 +14,14 @@ camera.position.z = 60;
 
 
 // LIGHTS
-// var light = new THREE.AmbientLight( 0x040404 ); // soft white light
-// scene.add( light );
-var spotLight = new THREE.SpotLight( 0xffffff );
-spotLight.position.set( 60, 50, 60 );
-spotLight.castShadow = true;
-spotLight.shadowCameraNear = 8;
-spotLight.shadowCameraFar = 30;
-spotLight.shadowMapWidth = 1024;
-spotLight.shadowMapHeight = 1024;
-spotLight.angle = 1;
-spotLight.exponent = 5;
-spotLight.shadowDarkness = 1;
-
+var light = new THREE.AmbientLight( 0xffffff ); // soft white light
+scene.add( light );
+var spotLight = new THREE.SpotLight( 0xaaaaaa );
+spotLight.position.set( 60, 100, 60 );
 spotLight.name = 'SpotLight';
 scene.add( spotLight );
+
+scene.fog = new THREE.FogExp2( 0x000000, 0.001 );
 
 //Set up Renderer
 renderer = new THREE.WebGLRenderer();
@@ -76,21 +69,25 @@ cubeGeometry = new THREE.BoxGeometry( 2, 2, 2);
 cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
 // scene.add( cube );
 for ( var i = 0; i < 100; i ++ ) {
+	var randomColor = '0x' + Math.floor(Math.random()*16777215).toString(16) ;
 	cubeMaterial = new THREE.MeshBasicMaterial( { name: 'material', color: 0xaaaaff } );
-	var phong = new THREE.MeshPhongMaterial( { color: 0x00ff00, specular: 0x666666, emissive: 0x0000aa, shininess: 1, shading: THREE.SmoothShading, opacity: 0.9, transparent: true });
+	var phong = new THREE.MeshPhongMaterial( {  specular: 0x111111, emissive: 0x040404, shininess: 100, shading: THREE.SmoothShading, opacity: 0.9, transparent: true });
 	var object = new THREE.Mesh( cubeGeometry, phong );
+	object.material.color.setHex(randomColor);
 
-	object.position.x = Math.random() * 50;
-	object.position.y = Math.random() * 50;
-	object.position.z = Math.random() * 50;
+	object.position.x = Math.random() * 500 - 250;
+	object.position.y = Math.random() * 500 - 250;
+	object.position.z = Math.random() * 500 - 250;
 
 	object.rotation.x = Math.random() * 2 * Math.PI;
 	object.rotation.y = Math.random() * 2 * Math.PI;
 	object.rotation.z = Math.random() * 2 * Math.PI;
 
-	object.scale.x = Math.random() + 0.5;
-	object.scale.y = Math.random() + 0.5;
-	object.scale.z = Math.random() + 0.5;
+	object.scale.x = Math.random() + 10;
+	object.scale.y = Math.random() + 10;
+	object.scale.z = Math.random() + 10;
+
+	object.name = 'CUBE';
 
 	scene.add( object );
 
@@ -115,10 +112,12 @@ function onDocumentMouseMove( event ) {
 
 			}
 
+
 var render = function () {
 	requestAnimationFrame( render );
+	TWEEN.update();
 
-	cube.rotation.x += 0.01;
+	// cube.rotation.x += 0.01;
 
 	theta += 0.1;
 
@@ -128,6 +127,20 @@ var render = function () {
 	// camera.lookAt( scene.position );
 
 	// camera.updateMatrixWorld();
+
+	var children = scene.children;
+	var target = { x : Math.random() * 400, y: Math.random() * 400 };
+	for ( var i = 0; i < children.length; i++ ) {
+		if (children[i].name === 'CUBE' && children[i].id % 2 === 0  ) {
+			
+			children[i].rotation.x += Math.random() * 0.01;
+
+			// children[i].material.color.setHex(randomColor);
+		} else if ( children[i].name === 'CUBE' && children[i].id % 2 === 0  ) {
+			children[i].rotation.y += Math.random() * 0.1;
+			children[i].rotation.x += Math.random() * 0.01;
+		}
+	}
 
 
 	// find intersections
@@ -162,8 +175,19 @@ var render = function () {
             }
 
             //touched object
-            INTERSECTED.position.x += 10;
-            console.log(INTERSECTED);
+            // INTERSECTED.position.x += 10;
+            new TWEEN.Tween( intersects[ 0 ].object.position ).to( {
+						x: Math.random() * 800 - 400,
+						y: Math.random() * 800 - 400,
+						z: Math.random() * 800 - 400 }, 4000 )
+					.easing( TWEEN.Easing.Elastic.Out).start();
+
+					new TWEEN.Tween( intersects[ 0 ].object.rotation ).to( {
+						x: Math.random() * 2 * Math.PI,
+						y: Math.random() * 2 * Math.PI,
+						z: Math.random() * 2 * Math.PI }, 2000 )
+					.easing( TWEEN.Easing.Elastic.Out).start();
+            // console.log(INTERSECTED);
         }
 
     } else {
