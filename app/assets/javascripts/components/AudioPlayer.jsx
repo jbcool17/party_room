b5185e5dff2ed96fa7b05a135abf5b798deffa5b
Robinson Lam <robinsonlam94@gmail.com>
2015-10-22 12:52:19 +1100

    Soundcloud track for CubeRoom

---
+++ app/assets/javascripts/components/AudioPlayer.jsx
@@ -0,0 +1,13 @@
+const AudioPlayer = React.createClass({
+	render(){
+		
+
+		return (
+			<div id="mp3_player">
+				<div id="audio_box"></div>
+					<canvas id="analyser_render"></canvas>
+			</div>
+		);
+	}
+});
+
